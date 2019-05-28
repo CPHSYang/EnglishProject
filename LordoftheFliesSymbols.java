4cc99e79832e45fa9a900c0e68411cf2857aa40a
CPHSYang <33789192+CPHSYang@users.noreply.github.com>
2019-05-28 11:53:25 -0700

    Create LordoftheFliesSymbols.java

---
+++ LordoftheFliesSymbols.java
@@ -0,0 +1,19 @@
+public Class LFSymbols { 
+
+// use slash marks to leave comments like this// 
+/* long comments are used like this and will not affect the code 
+*/ 
+
+/* put symbols and descrptions here for symbols in lord of the flies
+1. Conch (is pink, represents civilization, etc
+
+
+
+
+*/ 
+
+
+
+
+
+]
